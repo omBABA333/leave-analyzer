0edf071ccce3009f04473e3e6bee53b971491d94
Aher Om <aherom55@gmail.com>
2025-12-29 20:19:24 +0530

    cleaning

--- backend/routes/dashboard.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/:month/:employeeId', (req, res) => {
-  res.json({ message: 'Dashboard endpoint working!' });
-});
-
-module.exports = router;
